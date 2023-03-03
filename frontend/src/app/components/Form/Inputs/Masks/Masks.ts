27c16976a5be86ff9a4949eb3dc95950e80f5a75
Pedro Artur Buzzi Pereira <69548788+pedrobuzzJS@users.noreply.github.com>
2023-03-03 14:15:01 -0300

    Retornando eventos das máscaras

--- frontend/src/app/components/Form/Inputs/Masks/Masks.ts
+++ frontend/src/app/components/Form/Inputs/Masks/Masks.ts
@@ -1,11 +1,10 @@
-export function cep(e: React.FormEvent<HTMLInputElement>) {
+export function cep(e: React.FormEvent<HTMLInputElement>): React.FormEvent<HTMLInputElement> {
     e.currentTarget.maxLength = 9;
     let value = e.currentTarget.value;
     value = value.replace(/\D/g, "");
     value = value.replace(/^(\d{5})(\d)/, "$1-$2");
     e.currentTarget.value = value;
-    // return e;
-    return value;
+    return e;
   };
     
   export function currency(e: React.FormEvent<HTMLInputElement>) {
@@ -14,11 +13,10 @@ export function cep(e: React.FormEvent<HTMLInputElement>) {
     value = value.replace(/(\d)(\d{2})$/, "$1,$2");
     value = value.replace(/(?=(\d{3})+(\D))\B/g, ".");  
     e.currentTarget.value = value;
-    // return e;
-    return value;
+    return e;
   };
     
-  export function cpf(e: React.FormEvent<HTMLInputElement>) {
+  export function cpf(e: React.FormEvent<HTMLInputElement>): React.FormEvent<HTMLInputElement> {
     e.currentTarget.maxLength = 14;
     let value = e.currentTarget.value;
     if (!value.match(/^(\d{3}).(\d{3}).(\d{3})-(\d{2})$/)) {
@@ -28,14 +26,13 @@ export function cep(e: React.FormEvent<HTMLInputElement>) {
       value = value.replace(/(\d{3})(\d{2})$/, "$1-$2");
       e.currentTarget.value = value;
     };
-    // return e;
-    return value;
+    return e;
   };
 
   export function emailValidation(e: React.FormEvent<HTMLInputElement>) {
     return e.currentTarget.value;
   }
   
-  export function nullMask(e: React.FormEvent<HTMLInputElement>) {
-    return e.currentTarget.value;
+  export function nullMask(e: React.FormEvent<HTMLInputElement>): React.FormEvent<HTMLInputElement> {
+    return e;
   };
