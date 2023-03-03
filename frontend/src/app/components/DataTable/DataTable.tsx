255e1f639b9e275f1cfe6ee4059a701b9b4c00b8
Pedro Artur Buzzi Pereira <69548788+pedrobuzzJS@users.noreply.github.com>
2023-03-03 14:19:49 -0300

    Iniciando desenvolvimento de datatable

--- frontend/src/app/components/DataTable/DataTable.tsx
+++ frontend/src/app/components/DataTable/DataTable.tsx
@@ -1,7 +1,94 @@
-import React from "react";
+import React, { useCallback } from "react";
+import { useNavigate } from "react-router-dom";
+import { FieldTypes, GridFields } from "../../utils/Fields";
+import { Button } from "../Form/Button/Button";
+import { ButtonContainer, ButtonContainerGrid, Container, TableContainer, TableRows, TableSSS } from "./style";
 
-export const DataTable: React.FC = () => {
+interface GridProps {
+    columns: GridFields[],
+    initialData: any,
+    loading?: boolean,
+    pathMantencao?: string;
+};
+
+export const DataGrid: React.FC<GridProps> = ({ columns, initialData, loading, pathMantencao, ...props }) => {
+    const teste: any[] = initialData; 
+    const navigate = useNavigate();
+
+    const buildMaintenanceURL = useCallback( (btn: string, op: number, id?: any) => {
+        let url = btn + "-manutencao" + `/${op}` + `/${id}`;
+        return navigate("/"+url);
+    }, [navigate]);
+
+    const buttonInserFormValues = useCallback( (op: number) => {
+        let url = pathMantencao + "-manutencao" + `/${op}`;
+        return navigate("/"+url);
+    }, [navigate] );
+
     return (
-        <></>
-    );
+        // <Container>
+        //     {loading ? 
+        //         <h1>Carregando</h1> 
+        //         :
+        //         <>
+        //             <ButtonContainer>
+        //                 <Button
+        //                     buttonDescription="Inserir"
+        //                     size="prettySmall"
+        //                     onClick={() => buttonInserFormValues(
+        //                         2
+        //                     )}
+        //                 />
+        //             </ButtonContainer>
+        //             <TableContainer>
+        //                 <thead>
+        //                     <tr>
+        //                         {columns && columns.map( (column, index) => (
+        //                             <th key={index}>{column.title}</th>
+        //                         ) )}
+        //                     </tr>
+        //                 </thead>
+        //                 <tbody>
+        //                     {teste && teste?.map((item, index) => (
+        //                         <TableRows key={index} isOdd={Boolean(index%2)}>
+        //                             {columns && columns.map( (column, index) => (
+        //                             column.type === FieldTypes.TEXT ?
+        //                                 <td key={index}>
+        //                                     {item[column?.field]}
+        //                                 </td>
+        //                                 :
+        //                                 column.type === FieldTypes.BUTTON ?
+        //                                 <td key={index}>
+        //                                     <ButtonContainerGrid key={index}>
+        //                                         {column.buttons?.map((btn, index) => (
+        //                                             <Button
+        //                                                 key={index}
+        //                                                 size="prettySmall"
+        //                                                 buttonDescription={btn.title}
+        //                                                 // onClick={() => handdleSelectButtonActions(
+        //                                                 //     btn.button,
+        //                                                 //     btn.action,
+        //                                                 //     item.id,
+        //                                                 // )}
+                                                        
+        //                                             />
+        //                                         ))}
+        //                                     </ButtonContainerGrid>
+        //                                 </td>
+        //                                 :
+        //                                 <td key={index}></td>
+        //                         ) )}
+        //                         </TableRows>
+        //                     ))}
+        //                 </tbody>
+        //             </TableContainer>
+        //         </>
+        //     }
+        // </Container>
+        <>
+            <TableSSS>
+                
+            </TableSSS>
+        </>
+    )
 };
--- frontend/src/app/components/DataTable/style.ts
+++ frontend/src/app/components/DataTable/style.ts
@@ -1,1 +1,78 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
+
+export const Container = styled.div`
+    padding-top: 40px;
+    padding-bottom: 40px;
+`;
+
+export const TableContainer = styled.table`
+    width: 90%;
+    margin: 0 auto;
+    border-collapse: collapse;
+
+    th
+        {
+            padding: 10px;
+            background: #bcbcbc;
+            text-align: left;
+        }
+        
+    tbody {
+        width: 100%;
+    }
+`;
+
+interface TableRowProps {
+    isOdd?: boolean
+}
+
+export const TableRows = styled.tr<TableRowProps>`
+    text-align: left;
+    border-bottom: 1px solid #bcbcbc;
+    line-height: 15px;
+    background: ${props => props.isOdd? "#dddddd" : ""};
+
+    &:hover {
+        background: #b0acac;
+    }
+
+    td {
+        padding: 10px;
+        text-align: left;
+    }
+`;
+
+export const ButtonContainerGrid = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    flex-direction: row;
+    justify-content: center;
+`;
+
+export const ButtonContainer = styled.div`
+    width: 90%;
+    margin: 0 auto;
+    display: flex;
+    padding-bottom: 15px;
+    justify-content: right;
+`;
+
+export const FiltersContainer = styled.div`
+    width: 90%;
+    margin: 0 auto;
+    padding-bottom: 15px;
+`;
+
+
+export const TableSSS = styled.table`
+    margin: 0 0 40px 0;
+    width: 100%;
+    box-shadow: 0 1px 3px rgba(0,0,0,0.2);
+    display: table;
+    /* @media screen and (max-width: 580px); */
+    display: block;
+`;
+
+export const Row = styled.div`
+
+`;
