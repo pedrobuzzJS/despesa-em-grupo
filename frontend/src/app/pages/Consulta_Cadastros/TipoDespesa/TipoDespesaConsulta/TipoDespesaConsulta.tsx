f30b4e4e1600b00c310853e614c1575a6116ee03
Pedro Artur Buzzi Pereira <69548788+pedrobuzzJS@users.noreply.github.com>
2023-03-06 02:04:05 -0300

    Consulta de Tipo de Despesas

---
+++ frontend/src/app/pages/Consulta_Cadastros/TipoDespesa/TipoDespesaConsulta/TipoDespesaConsulta.tsx
@@ -0,0 +1,83 @@
+import React from "react";
+import { DataGrid } from "../../../../components/DataTable/DataTable";
+import { GridSysten } from "../../../../components/GridLayout/Grid/Grid";
+import { useFetch } from "../../../../hooks/useFetch";
+import { FieldTypes, GridFields } from "../../../../utils/Fields";
+import { Operation } from "../../../../utils/Operation";
+
+const campos: GridFields[] = [
+    {
+        field: "id",
+        title: "ID",
+        description: "id",
+        key: true,
+        type: FieldTypes.TEXT,
+    },
+    {
+        field: "nome",
+        title: "Nome",
+        description: "Nome",
+        type: FieldTypes.TEXT,
+    },
+    {
+        field: "descricao",
+        title: "Descrição",
+        description: "Descrição",
+        type: FieldTypes.TEXT,
+    },
+    {
+        field: "buttons",
+        title: "Acoes",
+        description: "Acoes",
+        type: FieldTypes.BUTTON,
+        buttons: [
+            {
+                button: "acoes",
+                action: Operation.DROP,
+                title: "",
+                icon: "RiIcons.RiDashboardFill",
+                rotina: "drop",
+                dropDownButtons: [
+                    {
+                        button: "tipo-despesa",
+                        action: Operation.VIEW,
+                        title: "Visualizar",
+                        icon: "RiDashboardFill",
+                        rotina: "tipo-despesa",
+                    },
+                    {
+                        button: "tipo-despesa",
+                        action: Operation.ALTER,
+                        title: "Alterar",
+                        icon: "RiDashboardFill",
+                        rotina: "tipo-despesa",
+                    },
+                    {
+                        button: "tipo-despesa",
+                        action: Operation.DELETE,
+                        title: "Deletar    ",
+                        icon: "RiDashboardFill",
+                        rotina: "tipo-despesa",
+                    },
+                ]
+            }
+        ]
+    }
+];
+
+export const TipoDespesaConsulta: React.FC = () => {
+    const { data } = useFetch("tipo-despesa");
+
+    return (
+        <>
+            <GridSysten container justify="center">
+                <GridSysten item cols={12}>
+                    <DataGrid 
+                        columns={campos} 
+                        initialData={data}
+                    />
+                </GridSysten>
+            </GridSysten>
+        </>
+    );
+};
