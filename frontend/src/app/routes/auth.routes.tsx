6e4b79b7408eba488ba36f053e3fa9bf969d823e
Pedro Artur Buzzi Pereira <69548788+pedrobuzzJS@users.noreply.github.com>
2023-03-06 02:04:59 -0300

    Novas rotas no front

--- frontend/src/app/routes/auth.routes.tsx
+++ frontend/src/app/routes/auth.routes.tsx
@@ -1,12 +1,11 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { DashBoard } from "../pages/Dashboard/Dashboard";
-import { PageA } from "../pages/PageA/PagaA";
 import { RoleProvider } from "../context/RoleContext";
 import { SideBarLayout } from "../components/Menu/SideBar/SideBar";
 import { MenuProvider } from "../context/menuContext";
 import { HomePage } from "../pages/Home/Home";
 import { SnackBarProvider } from "../context/snackBarContext";
 import { FormProvider } from "../context/formContext";
+import { TipoDespesaConsulta } from "../pages/Consulta_Cadastros/TipoDespesa/TipoDespesaConsulta/TipoDespesaConsulta";
 
 const AuthRoutes = () => {
     return (
@@ -17,8 +16,7 @@ const AuthRoutes = () => {
                         <FormProvider>
                             <Routes>
                                 <Route path="/" element={<RoleProvider children={<HomePage />} funcao={["Ser"]}/>}/>
-                                <Route path="dash" element={<RoleProvider children={<DashBoard />} funcao={["Ser"]}/>}/>
-                                <Route path="pagea" element={<RoleProvider children={<PageA />} funcao={["Rota Liberada"]}/>}/>
+                                <Route path="tipo-despesa" element={<RoleProvider children={<TipoDespesaConsulta />} funcao={["Ser"]}/>}/>
                                 <Route path="*" element={<h1>404</h1>} />
                             </Routes>
                         </FormProvider>
