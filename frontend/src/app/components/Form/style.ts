75d96c7363774bff4eafe63f3251acce3848b381
Pedro Artur Buzzi Pereira <69548788+pedrobuzzJS@users.noreply.github.com>
2023-03-06 02:03:21 -0300

    Estilizando formulário

--- frontend/src/app/components/Form/style.ts
+++ frontend/src/app/components/Form/style.ts
@@ -1,12 +1,20 @@
 import styled from "styled-components";
 
 export const FormContainer = styled.div`
-    width: 100%;
+    /* width: 100%;
     border: 1px solid var(--group-color);
     padding: 5px 10px 5px 10px;
     box-shadow: 0 0 5px #333;
-    margin-top: 10px;
-    margin-bottom: 10px;
+    margin-bottom: 10px; */
+    /* margin-top: 10px; */
+    width: 100%;
+    margin: 2rem 0;
+	padding: 1rem;
+	box-shadow: 0 0 5px #333;
+	border: solid 1px #fff;
+	background-color: #fff;
+	border-radius: 1rem;
+	position: relative;
 `;
 
 export const StyledForm = styled.form`
