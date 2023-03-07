740a66d7ef9baa29ff3fa31bae51c3332ac34192
Pedro Artur Buzzi Pereira <69548788+pedrobuzzJS@users.noreply.github.com>
2023-03-07 13:37:28 -0300

    Cadastro de tipo de despesa

--- frontend/src/app/pages/Consulta_Cadastros/TipoDespesa/TipoDespesaCadastro/TipoDespesaCadastro.tsx
+++ frontend/src/app/pages/Consulta_Cadastros/TipoDespesa/TipoDespesaCadastro/TipoDespesaCadastro.tsx
@@ -1,16 +1,20 @@
 import React from "react";
+import { useParams } from "react-router-dom";
 import { Form } from "../../../../components/Form/Form";
 import { GridSysten } from "../../../../components/GridLayout/Grid/Grid";
+import { useFetch } from "../../../../hooks/useFetch";
 import { FormInputs, InputType } from "../../../../utils/FormFields";
 
 const inputs: FormInputs[] = [
     {
+        key: true,
         name: "id",
         id: "id",
         label: "Código",
         placeholder: "Código",
         type: InputType.NUMBER,
-        cols: 4
+        cols: 4,
+        disabled: true,
     },
     {
         name: "nome",
@@ -18,7 +22,7 @@ const inputs: FormInputs[] = [
         label: "Nome",
         placeholder: "Nome",
         type: InputType.TEXT,
-        cols: 4
+        cols: 4,
     },
     {
         name: "descricao",
@@ -26,11 +30,18 @@ const inputs: FormInputs[] = [
         label: "Descrição",
         placeholder: "Descrição",
         type: InputType.TEXT,
-        cols: 4
+        cols: 4,
     },
 ];
 
 export const TipoDespesaCadastro: React.FC = () => {
+    const { op, id } = useParams();
+    const { data } = useFetch<any>("tipo-despesa", {
+        params: {
+            id: id
+        }
+    });
+
     return (
         <>
             <GridSysten
@@ -43,6 +54,8 @@ export const TipoDespesaCadastro: React.FC = () => {
                 >
                     <Form
                         campos={inputs}
+                        urlBakc="tipo-despesa"
+                        initialData={data}
                     />
                 </GridSysten>
             </GridSysten>
