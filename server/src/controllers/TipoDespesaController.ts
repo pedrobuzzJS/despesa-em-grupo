fb0492313ce701563d1c642d9977d6fc89571c66
Pedro Artur Buzzi Pereira <69548788+pedrobuzzJS@users.noreply.github.com>
2023-03-07 13:37:56 -0300

    Tipo despesa controller

--- server/src/controllers/TipoDespesaController.ts
+++ server/src/controllers/TipoDespesaController.ts
@@ -3,17 +3,84 @@ import { Request, Response } from "express";
 
 export default class TipoDespesaController {
     static async list(request: Request, response: Response) {
-        const data = await client.tipoDespesa.findMany({
-            orderBy: {
-                id: "asc"
-            }
-        });
+        const { id } = request.query;
+        console.log(id)
+        let data;
+
+        if (id) {
+            data = await client.tipoDespesa.findFirst({
+                where: {
+                    id: Number(id)
+                },
+                orderBy: {
+                    id: "asc"
+                }
+            });
+        } else {
+            data = await client.tipoDespesa.findMany({
+                orderBy: {
+                    id: "asc"
+                }
+            });
+        }
+
         if (!data) {
             return response.status(204).json({
                 "msg" : "Sem resultado"
             });
         } else {
             return response.json(data);
+        };
+    };
+
+    static async create(request: Request, response: Response) {
+        const { data } = request.body;
+        const objJson = JSON.parse(data);
+        try {
+            const tipoDespesa = await client.tipoDespesa.create({
+                data: {
+                    nome: objJson.nome,
+                    descricao: objJson.descricao 
+                }
+            });
+            if (tipoDespesa) {
+                return response.json(
+                    {
+                        "msg" : "Successes"
+                    }
+                );
+            }
+        } catch (error) {
+            return response.status(500).json({
+                message: error
+            });
+        };
+    };
+
+    static async update(request: Request, response: Response) {
+        const { data } = request.body;
+        const objJson = JSON.parse(data);
+        try {
+            const updateTipoDespesa = await client.tipoDespesa.update({
+                where: {
+                    id: Number(objJson.id)
+                },
+                data: {
+                    nome: objJson.nome,
+                    descricao: objJson.descricao,
+                }
+            });
+            if (updateTipoDespesa) {
+                return response.json(
+                    {
+                        "msg" : "Successes"
+                    }
+                );
+            };
+        } catch (error) {
+            return response.status(500).json({
+                message: error
+            });
         };
     };
 };
